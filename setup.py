4cc83a61c8932f0293c85ae2a46089ef109d7fe1
Cedric Renzi <cedric.renzi@laposte.net>
2022-07-26 19:38:57 +0200

    [IMP] adding setup and .gitignore

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="AWP",
+    use_scm_version=True,
+    description="",
+    license="",
+    author="",
+    author_email="",
+    url="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+    ],
+    extras_require={
+        "dev": [
+        ],
+    },
+    setup_requires=["setuptools_scm"],
+    #entry_points={"console_scripts": ["python = app.run:main"]},
+)
